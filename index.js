90774a19c23cb7c2a7dc89356105287c040c5470
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-31 17:26:46 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+//this is a js file
+//joao paulo
+
+//1.
+//a.10 ;50
+//b.no console nao vai imprimir nada
+
+//2
+//a.essa funçao serve para criar uma variavel chamada "textodousuario"
+//b.determinaria usando o codigo return
+
+//a. escrita de código
+
+function informaçãodousuario() {
+    console.log("eu sou joao paulo,moro em sao leopoldo e sou estudante.");
+}
+//b
+function imprimirinfo(nome,idade,endereço,profissao){
+return "eu sou" + nome, "tenho " + idade + " anos," + "moro em " + endereço + "e sou " + profissao;
+}
+console.log(imprimirinfo('joao paulo','16', 'sao leopoldo','estudante'));
+
+//
+
+
